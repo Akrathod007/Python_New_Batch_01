d7ec30c6558a5fa253cedb7158eebc09a286dbb8
Akrathod007 <anshrathod2104@gmail.com>
2026-05-21 17:19:09 +0530

    Added

--- list.py
+++ list.py
@@ -57,7 +57,7 @@ print(a is c)
 print(a is d)
 
 """
-
+"""
 
 l7 = [1, 2, 3, 4, 5, 4, 6, 5, 4, 7, 8, 9, 1, 2]
 
@@ -117,3 +117,189 @@ print(l9)
 
 # del l9
 # print(l9)
+
+
+"""
+
+
+# l7 = [1, 2, 3, 4, 5, 4, 6, 5, 4, 7, 8, 9, 1, 2]
+
+# # for i in range(len(l7)):
+# #     print(i, "->", l7[i])
+
+# for i in l7:
+#     print(i)
+
+
+"""
+1. Find Even and Odd Numbers from a List
+l1 = [1,2,3,4,5,6,7,8,9,10]
+odd = [1,3,5,7,9]
+even = [2,4,6,8,10]
+"""
+"""
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd = []
+even = []
+
+for i in l1:
+    if i % 2 == 0:
+        even.append(i)
+    else:
+        odd.append(i)
+
+print("Even :", even)
+print("Odd :", odd)
+
+"""
+
+"""
+2. Remove Duplicates from a List
+l1 = [1,2,3,1,2,4,5,3,2]
+output = result = [1,2,3,4,5]
+"""
+
+
+"""
+l1 = [1, 2, 3, 1, 2, 4, 5, 3, 2]
+unique = []
+
+for i in l1:
+    if i not in unique:
+        unique.append(i)
+
+print("Unique :", unique)
+
+"""
+
+
+"""
+3. Find Common Elements Between Two Lists
+l1 = [1,2,3,4]
+l2 = [3,4,5,6]
+
+result = [3,4]
+"""
+
+"""
+l1 = [1, 2, 3, 4]
+l2 = [3, 4, 5, 6]
+result = []
+for i in l1:
+    if i in l2:
+        result.append(i)
+
+print("Result :", result)
+
+"""
+
+"""
+
+4. Remove All Even Numbers from a List
+l1 = [1,2,3,4,5,6]
+result = [1,3,5]
+"""
+
+"""
+l1 = [1, 2, 3, 4, 5, 6]
+result = []
+
+for i in l1:
+    if i % 2 != 0:
+        result.append(i)
+
+print("Result :", result)
+
+"""
+
+"""
+5. Find Elements Present in One List but Not in the Other
+l1 = [1,2,3,4]
+l2 = [3,4,5,6]
+
+result = [1,2,5,6]
+"""
+
+"""
+l1 = [1, 2, 3, 4]
+l2 = [3, 4, 5, 6]
+
+result = []
+
+for i in l1:
+    if i not in l2:
+        result.append(i)
+
+for i in l2:
+    if i not in l1:
+        result.append(i)
+
+print("Result :", result)
+
+"""
+
+"""
+6. Check if a List is a Palindrome
+l1 = [1,2,3,2,1]
+rev = [1,2,3,2,1]
+"""
+
+
+"""
+l1 = [1, 2, 3, 2, 1, 6]
+rev = []
+
+for i in range(len(l1) - 1, -1, -1):
+    rev.append(l1[i])
+
+print(l1)
+print(rev)
+
+if l1 == rev:
+    print("Palindrome List")
+else:
+    print("Not Palindrome List")
+
+"""
+
+"""
+7. Find All Unique Elements in a List
+l1 = [1,2,3,1,2,4,5,3,2]
+result = [4,5]
+"""
+
+
+"""
+l1 = [1, 2, 3, 1, 2, 4, 5, 3, 2]
+
+result = []
+
+for i in l1:
+    if l1.count(i) == 1:
+        result.append(i)
+
+print("Result :", result)
+
+"""
+
+"""
+8. Move All Zeros to the End 
+l1 = [1,0,0,2,3,0,2]
+
+result = [1,2,3,2,0,0,0]
+"""
+
+l1 = [1, 0, 0, 2, 3, 0, 2]
+
+result = []
+
+for i in l1:
+    if i != 0:
+        result.append(i)
+
+zeroCount = l1.count(0)
+
+for i in range(1, zeroCount + 1):
+    result.append(0)
+
+print(result)
