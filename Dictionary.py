ccc3499058f25d2a6ed1bf36432e2074a7d00300
Akrathod007 <anshrathod2104@gmail.com>
2026-05-26 16:37:41 +0530

    Task Added

--- Dictionary.py
+++ Dictionary.py
@@ -62,7 +62,7 @@ for key in user:
 # student.update({"age": 22, "city": "Surat"})
 # print(student)
 
-
+"""
 student = {"name": "Raju", "age": 21}
 new_student = student.copy()
 print(new_student)
@@ -118,3 +118,259 @@ details = {
 print(details)
 # print(details.get("address")["city"])
 print(details.get("address").get("city"))
+
+"""
+
+"""
+1. Count Frequency of Characters
+word = "python"
+
+freq = {
+    "p":1
+}
+
+2. Count Frequency of Words
+sentence = "python is easy python is powerful"
+
+3. Create Dictionary of Number and Cube
+data = {}
+
+data = {
+    1:1,
+    2:8,
+    3:27
+}
+
+4. Swap Keys and Values
+data = {"a": 1,"b": 2, "c": 3}
+
+5. Find Duplicate Values
+data = {
+    "a": 10,
+    "b": 20,
+    "c": 10,
+    "d": 30
+}
+
+6. Create Dictionary From String Length
+words = ["python", "java", "ai"]
+
+data = {
+    "Python":6
+}
+
+7. Count Vowels in Sentence
+sentence = "python programming"
+
+8. Create Dictionary of Factorials
+data = {
+    1:1,
+    2:2,
+    3:6,
+    4:24,
+    5:120
+}
+
+9. Group Numbers by Positive and Negative
+numbers = [10, -5, 7, -2, 0]
+
+data = {
+    "positive": [],
+    "negative": [],
+    "zero": []
+}
+
+10. Separate Even and Odd Numbers
+numbers = [1, 2, 3, 4, 5, 6]
+
+data = {
+    "even": [],
+    "odd": []
+}
+
+"""
+
+"""
+1. Count Frequency of Characters
+word = "python"
+"""
+"""
+word = "madam"
+
+freq = {}
+
+for ch in word:
+    freq[ch] = freq.get(ch, 0) + 1
+
+print(freq)
+
+"""
+
+
+"""
+2. Count Frequency of Words
+sentence = "python is easy python is powerful"
+"""
+"""
+sentence = "python is easy python is powerful"
+
+words = sentence.split()
+print(words)
+
+freq = {}
+for word in words:
+    freq[word] = freq.get(word, 0) + 1
+
+print(freq)
+
+"""
+
+
+"""
+3. Create Dictionary of Number and Cube
+data = {}
+"""
+"""
+no = int(input("Enter a number : "))
+data = {}
+
+for i in range(1, no + 1):
+    data[i] = i**3
+
+print(data)
+
+"""
+
+
+"""
+4. Swap Keys and Values
+data = {"a": 1,"b": 2, "c": 3}
+"""
+"""
+data = {"a": 1, "b": 2, "c": 3}
+
+new_data = {}
+
+for k, v in data.items():
+    new_data[v] = k
+
+print(new_data)
+"""
+
+
+"""
+5. Find Duplicate Values
+data = {
+    "a": 10,
+    "b": 20,
+    "c": 10,
+    "d": 30
+}
+"""
+
+"""
+data = {"a": 10, "b": 20, "c": 10, "d": 20, "e": 30, "f": 20, "g": 10, "h": 40}
+
+duplicate = []
+
+values = list(data.values())
+print(values)
+
+for i in values:
+    if values.count(i) > 1 and i not in duplicate:
+        duplicate.append(i)
+
+print(duplicate)
+
+"""
+
+
+"""
+6. Create Dictionary From String Length
+words = ["python", "java", "ai"]
+"""
+
+"""
+words = ["python", "java", "ai"]
+
+data = {}
+
+for word in words:
+    data[word] = len(word)
+
+print(data)
+"""
+
+
+"""
+7. Count Vowels in Sentence
+sentence = "python programming"
+"""
+"""
+sentence = "hello python and hello javascript"
+data = {}
+
+for ch in sentence:
+    if ch in "AEIOUaeiou":
+        data[ch] = data.get(ch, 0) + 1
+
+print(data)
+
+"""
+
+
+"""
+8. Create Dictionary of Factorials
+"""
+"""
+no = int(input("Enter a number : "))
+
+data = {}
+
+for i in range(1, no + 1):
+    fact = 1
+    for j in range(1, i + 1):
+        fact = fact * j
+
+    data[i] = fact
+
+print(data)
+
+"""
+
+
+"""
+9. Group Numbers by Positive and Negative
+numbers = [10, -5, 7, -2, 0]
+
+data = {
+    "positive": [],
+    "negative": [],
+    "zero": []
+}
+
+10. Separate Even and Odd Numbers
+numbers = [1, 2, 3, 4, 5, 6]
+
+data = {
+    "even": [],
+    "odd": []
+}
+
+"""
+
+
+numbers = [10, -5, 7, -2, 0]
+
+data = {"positive": [], "negative": [], "zero": []}
+
+
+for i in numbers:
+    if i > 0:
+        data["positive"].append(i)
+    elif i < 0:
+        data["negative"].append(i)
+    else:
+        data["zero"].append(i)
+
+print(data)
