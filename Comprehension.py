508b5b53751d1e16c1ea7f3f40dbb59d945bd7f7
Akrathod007 <anshrathod2104@gmail.com>
2026-05-28 17:52:00 +0530

    Added

--- Comprehension.py
+++ Comprehension.py
@@ -27,15 +27,15 @@ print(even2)
 
 """
 
-evenOdd = []
+# evenOdd = []
 
-for i in range(1, 11):
-    if i % 2 == 0:
-        evenOdd.append("Even")
-    else:
-        evenOdd.append("Odd")
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         evenOdd.append("Even")
+#     else:
+#         evenOdd.append("Odd")
 
-print(evenOdd)
+# print(evenOdd)
 
 # evenOdd2 = ["Even" if i % 2 == 0 else "Odd" for i in range(1, 11)]
 # li = [34, 234, 456, 123, 67, 234, 78, 23, 44, 55, 11]
@@ -49,28 +49,28 @@ print(evenOdd)
 # posNegZero = ["Pos" if i > 0 else "Neg" if i < 0 else "zero" for i in nums]
 # print(posNegZero)
 
-marks = [90, 98, 78, 85, 45, 67, 75, 30, 40]
+# marks = [90, 98, 78, 85, 45, 67, 75, 30, 40]
 
-grade = [
-    (
-        "A"
-        if i >= 90
-        else (
-            "B"
-            if i >= 80
-            else "C" if i >= 70 else "D" if i >= 60 else "E" if i >= 50 else "Fail"
-        )
-    )
-    for i in marks
-]
+# grade = [
+#     (
+#         "A"
+#         if i >= 90
+#         else (
+#             "B"
+#             if i >= 80
+#             else "C" if i >= 70 else "D" if i >= 60 else "E" if i >= 50 else "Fail"
+#         )
+#     )
+#     for i in marks
+# ]
 
-print(grade)
+# print(grade)
 
 
-c = [10, 45, 23, 67, 30]
-# f = c * 1.8 + 32
-f = [i * 1.8 + 32 for i in c]
-print(f)
+# c = [10, 45, 23, 67, 30]
+# # f = c * 1.8 + 32
+# f = [i * 1.8 + 32 for i in c]
+# print(f)
 
 
 """
@@ -94,3 +94,138 @@ Task 4 : Marks → "Pass" if ≥ 40 else "Fail"
 Input: [35, 50, 90, 20]
 Output: ["Fail", "Pass", "Pass", "Fail"]
 """
+
+
+"""
+
+no -> 10
+
+li = []
+
+
+"""
+
+# no = int(input("how many elements do you want : "))
+# li = []
+
+# for i in range(1, no + 1):
+#     li.append(int(input("Enter a value : ")))
+
+# print(li)
+
+
+# l2 = [int(input("Enter a value : ")) for i in range(1, no + 1)]
+# print(l2)
+
+
+# r = int(input("Enter a row : "))
+# c = int(input("Enter a column"))
+
+"""
+li = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+"""
+# ol = []
+# for i in range(1, r + 1):
+#     il = []
+#     for j in range(1, c + 1):
+#         no = int(input("Enter a number : "))
+#         il.append(no)
+#         # [2,3,4]
+#     ol.append(il)
+
+# print(ol)
+
+
+# l2 = [
+#     [int(input("Enter a number : ")) for j in range(1, c + 1)] for i in range(1, r + 1)
+# ]
+
+# print(l2)
+
+
+# s = {i * i for i in range(1, 11)}
+# print(s)
+
+# s2 = {i * i for i in range(1, 11) if i % 2 == 0}
+# print(s2)
+
+# li = [325, 214, 46, 24, 66, 77, 89, 34]
+# s3 = {"even" if i % 2 == 0 else "Odd" for i in li}
+# print(s3)
+
+
+# d = {i: i * i for i in range(1, 11)}
+# print(d)
+
+# d = {i: i * i for i in range(1, 11) if i % 2 == 0}
+# print(d)
+
+# d2 = {i: "Even" if i % 2 == 0 else "odd" for i in range(1, 11)}
+# print(d2)
+
+"""
+1. Create a dictionary from 1 to 10, but include only even numbers as keys and their cubes as values.
+
+2. Given a list of words, create a dictionary where key = word and value = length.
+
+3. Create a dictionary from 1 to 10:
+
+Even → "Even"
+Odd → "Odd"
+
+4. Swap keys and values of a dictionary.
+
+5. Create a dictionary using two lists.
+
+keys = ["name", "age", "city"]
+values = ["Ram", 11, "Surat"]
+
+6. Count frequency of each character in a string.
+
+
+"""
+
+# 2. Given a list of words, create a dictionary where key = word and value = length.
+
+# words = ["Apple", "Banana", "Mango", "Kiwi", "Watermelon"]
+
+# # li = {w: len(w) for w in words}
+# li = {w: w[::-1] for w in words}
+# print(li)
+
+# 4. Swap keys and values of a dictionary.
+
+# d = {"a": 1, "b": 2, "c": 3}
+
+# d1 = {v: k for k, v in d.items()}
+# print(d1)
+
+
+"""
+
+5. Create a dictionary using two lists.
+
+keys = ["name", "age", "city"]
+values = ["Ram", 11, "Surat"]
+"""
+"""
+
+keys = ["name", "age", "city"]
+values = ["Ram", 11, "Surat"]
+
+
+d = {keys[i]: values[i] for i in range(len(keys))}
+print(d)
+
+"""
+
+# 6. Count frequency of each character in a string.
+
+s = "Mississippi"
+
+d = {ch: s.count(ch) for ch in s}
+print(d)
---
+++ userDefinedFunction.py
@@ -0,0 +1,227 @@
+# Function is block of code
+
+"""
+prime(){
+
+    //code
+}
+
+Type of UDF:
+
+1 no return type no arguments
+2 no return type with arguments
+3 return type no arguments
+4 return type with arguments
+
+"""
+
+# 1 no return type no arguments
+
+
+# def sayHi():
+#     print("Hello")
+
+
+# sayHi()
+# sayHi()
+# sayHi()
+# sayHi()
+# sayHi()
+
+
+# 2 no return type with arguments
+
+
+# def add(a, b):
+#     print(f"{a} + {b} = {a + b}")
+
+
+# add(10, 20)
+# add(30, 20)
+# add(100, 125)
+
+
+# 3 with return type no arguments
+
+
+# def mul():
+#     no1 = int(input("Enter a number 1 : "))
+#     no2 = int(input("Enter a number 2 : "))
+#     # print(no1 * no2)
+
+#     # return 0
+#     return no1 * no2
+
+
+# x = mul()
+# print(x)
+
+# print(mul())
+
+
+# 4 with return type with arguments
+
+"""
+def isPrime(no):
+    f = 0
+    for i in range(1, no + 1):
+        if no % i == 0:
+            f += 1
+
+    if f == 2:
+        return 1
+    else:
+        return 0
+
+
+# print(f)
+no = int(input("Enter a number : "))
+x = isPrime(no)
+
+if x == 1:
+    print("No is Prime")
+else:
+    print("No is not prime")
+
+"""
+
+
+# def toUpper():
+#     name = input("Enter a name : ")
+
+#     return name.upper()
+
+
+# x = toUpper()
+# print(x)
+
+
+# li = [1, 2, 3, 4, 5]
+# li2 = [1, 2, 3, 4, 5]
+# li3 = ["Hello", "Hi", "Bye"]
+
+
+# def printList(l):
+#     for i in l:
+#         print(i)
+
+
+# printList(li)
+# printList(li2)
+# printList(li3)
+
+
+# l1 = [1, 2, 3, 4]
+# l2 = [3, 4, 5, 6]
+
+
+# def commonElm(li1, li2):
+#     result = []
+#     for i in li1:
+#         if i in li2:
+#             result.append(i)
+
+#     return result
+
+
+# ans = commonElm(l1, l2)
+# print(ans)
+
+"""
+def printTupel(t):
+    for i in t:
+        print(i)
+
+
+t1 = (1, 2, 3, 4, 5)
+printTupel(t1)
+
+"""
+
+"""
+def uniqueElm(l):
+    s = set(l)
+
+    return s
+
+
+x = uniqueElm([1, 2, 3, 4, 5, 1, 2, 3, 6, 5, 7, 4, 3, 2])
+print(x)
+
+"""
+"""
+words = ["python", "java", "ai"]
+
+
+def strLength(li):
+    data = {}
+
+    for i in li:
+        data[i] = len(i)
+
+    return data
+
+
+x = strLength(words)
+print(x)
+
+"""
+
+
+def Math(a, b):
+
+    return a + b, a - b, a * b, a / b, a % b, a // b, a**b
+
+
+# print(Math(10, 20))
+# add, sub, mul, div, mod, fdiv, power = Math(10, 20)
+
+# print(add)
+# print(sub)
+# print(mul)
+# print(div)
+# print(mod)
+# print(fdiv)
+# print(power)
+
+"""
+def demo():
+    return (
+        10,
+        1.2,
+        "Hello",
+        True,
+        [1, 2, 3, 4],
+        (1, 2, 3),
+        {1, 2, 3},
+        {1: 1, 2: 4, 3: 9},
+    )
+
+
+print(demo())
+
+"""
+
+
+# def add(a, b=10, c=20):
+#     print(a + b)
+
+
+# add(10)
+# add(10, 50)
+
+
+# def add(a, b, c):
+#     print(a + b + c)
+
+
+# add(c=10, a=20, b=30)
+
+
+def add(a, b, *args):
+    print(a)
+    print(b)
+    print(args)
+
+
+add(10, 20, 30, 40, 50, 60)
